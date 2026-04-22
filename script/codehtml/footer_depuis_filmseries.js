b345498a74e011d8dfc425b4f02429aedf3ef296
MrFlake0304 <mr.flake0304@gmail.com>
2026-04-22 15:52:00 +0200

    mentions légales + flooter a chaque films et serie

--- script/codehtml/footer_depuis_filmseries.js
+++ script/codehtml/footer_depuis_filmseries.js
@@ -1,32 +1,31 @@
 document.querySelector('.footer_script').innerHTML = `
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 
-            <br> <br>
+    <br> <br>
 
-            <hr>
+    <hr>
 
-            <div class="footer">
-                <div class="footer-icones">
-                    <a href="https://www.facebook.com/netflixfrance"><i class="fab fa-facebook"></i></a>
-                    <a href="https://www.instagram.com/NetflixFR"><i class="fab fa-instagram"></i></a>
-                    <a href="https://x.com/NetflixFR"><i class="fab fa-twitter"></i></a>
-                    <a href="https://www.youtube.com/user/netflixfrance"><i class="fab fa-youtube"></i></a>
-                </div>
-
-                <div class="footer-text">
-                    <a href="../../apropo.html">A propos</a>
-                    <a href="../../mentions_legales.html">Mentions légales</a>
-                    <a href="../../conditionsutilisation.html">Conditions d'utilisation</a>
-                </div>
-            </div>
-
-            <footer>
-                <div>
-                    <h1>AYLOUFLIX</h1>
-                    <p>© 2024 AYLOUFLIX - Tous droits réservés sur des œuvres qui n'existent pas.</p>
-                    <p>67 rue de Vitality, 75001 Paris | contact@aylouflix.fr</p>
-                    <p class="sous-text-footer">Projet éducatif SNT — Document fictif à but pédagogique</p>
-                </div>
-            </footer>
+    <div class="footer">
+        <div class="footer-icones">
+            <a href="https://www.facebook.com/netflixfrance"><i class="fab fa-facebook"></i></a>
+            <a href="https://www.instagram.com/NetflixFR"><i class="fab fa-instagram"></i></a>
+            <a href="https://x.com/NetflixFR"><i class="fab fa-twitter"></i></a>
+            <a href="https://www.youtube.com/user/netflixfrance"><i class="fab fa-youtube"></i></a>
         </div>
+
+        <div class="footer-text">
+            <a href="../../apropo.html">A propos</a>
+            <a href="../../mentions_legales.html">Mentions légales</a>
+            <a href="../../conditionsutilisation.html">Conditions d'utilisation</a>
+        </div>
+    </div>
+
+    <footer>
+        <div>
+            <h1>AYLOUFLIX</h1>
+            <p>© 2024 AYLOUFLIX - Tous droits réservés sur des œuvres qui n'existent pas.</p>
+            <p>67 rue de Vitality, 75001 Paris | contact@aylouflix.fr</p>
+            <p class="sous-text-footer">Projet éducatif SNT — Document fictif à but pédagogique</p>
+        </div>
+    </footer>
 `;
